a10fb1bad4cad0181f0d1cc923e013944b190e7e
WR3CKk <sakimarasakirururu@gmail.com>
2021-10-28 03:12:46 +0300

    first commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Alexander on 07.02.2021.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
